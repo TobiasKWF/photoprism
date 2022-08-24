81802fcd5c14358b0256ba615624931fc97213d3
TobiasK <info@tobiaskoenig.de>
2022-08-24 10:12:39 +0200

    Update tensorflow-driver.sh

--- scripts/dist/tensorflow-driver.sh
+++ scripts/dist/tensorflow-driver.sh
@@ -4,8 +4,8 @@ PATH="/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:/scripts:$PAT
 
 CPU_DETECTED=$(lshw -c processor -json 2>/dev/null)
 
-if [[ $(echo "${CPU_DETECTED}" | jq -r '.[].capabilities.avx2') == "true" ]]; then
+if [[ $(echo "${CPU_DETECTED}" | jq -r '.[0].capabilities.avx2') == "true" ]]; then
   echo "avx2"
-elif [[ $(echo "${CPU_DETECTED}" | jq -r '.[].capabilities.avx') == "true" ]]; then
+elif [[ $(echo "${CPU_DETECTED}" | jq -r '.[0].capabilities.avx') == "true" ]]; then
   echo "avx"
 fi
